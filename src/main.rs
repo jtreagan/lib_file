8a6ad101dd7d509c8309abd62a02937d7be0ac5d
John T. Reagan <john@ramblingreagans.com>
2024-07-30 09:28:03 -0700

    Just created three functions using FLTK's dialog crate to return path and file name Strings.

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,63 @@
 /*
     ~~~~~~~~~~~~~~~~~~~~~~~  Notes  ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    -- You will eventually want to include choosing the directory
-        you want to pull the files from.
 
  */
 
 /*
     ~~~~~~~~~~~~~~~~~~~~~~~  Goals  ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    -- Take a vector of file names
-
-        -- Eliminate unwanted extensions
-
+    -- Working on    pathonly()
 
  */
 
 
 use lib_file::file_mngmnt::*;
+use fltk::*;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::convert::From;
 
 const PROGRAM_TITLE: &str = "File Management Functions";
 const VERSION: &str = "0.1.5";
-
-use std::convert::From;
-
 
 fn splash() {
     println!("~~~~~~~~~~~~~~  {}  ~~~~~~~~~~~~~~~~", PROGRAM_TITLE);
     println!("                  VERSION   {}\n\n", VERSION);
 }
 
-
 fn main() {
-    let path = ("/home/camascounty/programming/rust/mine/file_lib").to_string();
-    let dirpath = file_choose_new_fname("vrbl", &path);
-    println!("\nIn main() the new path & fname is:  {}", dirpath);
 
-    println!("\n All is okay!!  :>) \n");
+    let path = file_pathonly();
+
+    println!("\n {:?} \n", path);
+
 }
 
+
+
+
+pub fn file_pathonly() -> String {
+    let mut dialog = dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseFile);
+    dialog.show();
+    println!("\n {:?} \n", dialog.filename());
+
+    let path = dialog.filename().to_str().unwrap().to_string();
+
+    path
+}
+
+
+pub fn file_nameonly() -> String {
+    let mut dialog = dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseFile);
+    dialog.show();
+    println!("\n {:?} \n", dialog.filename());
+
+    let path = dialog.filename();
+
+    let filename = path.file_name().expect("The path has no file available.");
+    let filename_str = filename.to_str().expect("The path is not valid UTF-8");
+    let filename_string: String = filename_str.to_string();
+
+    filename_string
+}
